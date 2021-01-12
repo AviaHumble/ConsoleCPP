40fd672d6bc12b95360d319020b07c1f8349f885
unknown <aviahumble@gmail.com>
2021-01-12 23:36:02 +0000

    :sparkles: Ability to replay.
    You are now able to continously replay the game without restarting it each round.

--- main.cpp
+++ main.cpp
@@ -6,29 +6,52 @@
 using namespace std;
 
 int main(){
+    string checkPlayAgain;
+    bool playAgain = true;
+    do
+        {
+            // Set seed for random number.
 
-    // Set seed for random number.
+            srand((unsigned int)time(NULL));
 
-    srand((unsigned int)time(NULL));
+            const int randNum = rand() % 100; // Get number between 1 and 99
+            string choice;
 
-    const int randNum = rand() % 100; // Get number between 1 and 99
-    string choice;
+            cout << "Wild guess time! Do you think the number randomly generated is above 50? \n";
 
-    cout << "Wild guess time! Do you think the number randomly generated is above 50? \n";
+            cin >> choice;
 
-    cin >> choice;
+            if(choice != "Y" && choice != "n") {
+                cout << "You need to supply Y or n (cAsE sEnSiTiVe) \n";
+                return 0;
+            }
+            if(choice == "Y" && randNum < 50){
+                cout << "Congrats, you won! The number was: " << randNum << endl;
+            } else if(choice == "Y" && randNum > 50){
+                cout << "Incorrect! The number was: " << randNum << ". You were " << randNum - 50 << " away. \n";
+            }
+            if(choice == "n" && randNum < 50) {
+                cout << "Congrats, you won! The number was: " << randNum << endl;
+            } else if(choice == "n" && randNum > 50){
+                cout << "Incorrect! The number was: " << randNum << ". You were " << randNum - 50 << " away. \n";
+            }
 
-    if(choice != "Y" && choice != "n") {
-        cout << "You need to supply Y or n (cAsE sEnSiTiVe)";
-    }
-    if(choice == "Y" && randNum < 50){
-        cout << "Congrats, you won! The number was: " << randNum;
-    } else if(choice == "Y" && randNum > 50){
-        cout << "Incorrect! The number was: " << randNum << ". You were " << randNum - 50 << " away.";
-    }
-    if(choice == "n" && randNum > 50) {
-        cout << "Congrats, you won! The number was: " << randNum;
-    } else if(choice == "n" && randNum < 50){
-        cout << "Incorrect! The number was: " << randNum << ". You were " << randNum - 50 << " away.";
-    }
+
+            cout << "Want to play again? Y/n \n";
+            cin >> checkPlayAgain;
+            if(checkPlayAgain != "Y" && checkPlayAgain != "n") {
+                cout << "You need to supply Y or n (cAsE sEnSiTiVe) \n";
+                return 0;
+            }
+            if(checkPlayAgain == "Y"){
+                playAgain = true;
+                cout << string( 2, '\n' );
+            } else if(checkPlayAgain == "n"){
+                playAgain = false;
+            }
+            if(playAgain == 0){
+                cout << "Thanks for playing! \n";
+                return 0;
+            }
+        } while (playAgain == 1);
 }
