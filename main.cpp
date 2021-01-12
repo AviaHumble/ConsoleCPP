79ba74cbaf11eced89193685a9806c3a09d4885d
unknown <aviahumble@gmail.com>
2021-01-12 22:41:17 +0000

    :sparkles: New Game!
    First console game of my C++ series.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 int main(){
-    cout << "subway sexist";
-    return 0;
-};
+
+    // Set seed for random number.
+
+    srand((unsigned int)time(NULL));
+
+    const int randNum = rand() % 100; // Get number between 1 and 99
+    string choice;
+
+    cout << "Wild guess time! Do you think the number randomly generated is above 50? \n";
+
+    cin >> choice;
+
+    if(choice != "Y" && choice != "n") {
+        cout << "You need to supply Y or n (cAsE sEnSiTiVe)";
+    }
+    if(choice == "Y" && randNum < 50){
+        cout << "Congrats, you won! The number was: " << randNum;
+    } else if(choice == "Y" && randNum > 50){
+        cout << "Incorrect! The number was: " << randNum << ". You were " << randNum - 50 << " away.";
+    }
+    if(choice == "n" && randNum > 50) {
+        cout << "Congrats, you won! The number was: " << randNum;
+    } else if(choice == "Y" && randNum < 50){
+        cout << "Incorrect! The number was: " << randNum << ". You were " << randNum - 50 << " away.";
+    }
+}
